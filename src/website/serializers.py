aa5297f4b7cfc621b32222fedaf7c09b691ca082
sealwing <sealinsky@gmail.com>
2021-12-21 13:33:28 +0500

    Website admin content manager, basic API

---
+++ src/website/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from website.models import Product, Category, Order, FeedbackComment
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+# pylint: disable-next=abstract-method
+class CartItemSerializer(serializers.Serializer):
+    product_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    amount = serializers.IntegerField(min_value=1)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    cart = CartItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        exclude = ["created_at"]
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FeedbackComment
+        exclude = ["created_at"]
---
+++ src/website/views.py
@@ -0,0 +1,48 @@
+from rest_framework import viewsets, views
+from rest_framework.response import Response
+
+from website.models import Product, Category, FeedbackComment, CartItem, Order
+from website.serializers import (
+    ProductSerializer,
+    CategorySerializer,
+    OrderSerializer,
+    FeedbackSerializer,
+)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderView(views.APIView):
+    http_method_names = ["post"]
+
+    # pylint: disable-next=no-self-use
+    def post(self, request):
+        serializer = OrderSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        cart = serializer.validated_data.pop("cart")
+        order = Order(**serializer.validated_data)
+        order.save()
+        CartItem.objects.bulk_create(
+            [CartItem(order=order, product=item["product_id"], amount=item["amount"]) for item in cart]
+        )
+        return Response()
+
+
+class FeedbackView(views.APIView):
+    http_method_names = ["post"]
+
+    # pylint: disable-next=no-self-use
+    def post(self, request):
+        serializer = FeedbackSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        FeedbackComment(**serializer.validated_data).save()
+        return Response()

7551bf575c3cb37af8a30e33f1d059e89f45a3e7
sealwing <sealinsky@gmail.com>
2021-12-28 19:25:43 +0500

    Mailing init

--- src/messenger_bot/services/notifiers.py
+++ src/messenger_bot/services/notifiers.py
@@ -4,49 +4,14 @@ from telebot import types
 
 from messenger_bot.models import BotUser
 from messenger_bot.services.decorators import log_any_exception
-from services.store import calculate_price
+from services.store import form_order_text_repr
 from website.models import Order, FeedbackComment
 
 
 @log_any_exception()
 def notify_new_order(order: Order, bot: telebot.TeleBot):
     users_to_notify = BotUser.objects.filter(chat_id__isnull=False, notifies_on=True).all()
-    cart_items = order.cartitem_set.prefetch_related("product").all()
-    order_price = calculate_price(cart_items)
-    message = "\n".join(
-        (
-            f"Поступил новый заказ: №{order.id}",
-            "",
-            f"От: {order.name}",
-            "",
-            "Контакты:",
-            f"Телефон: {order.phone}",
-            f"E-mail: {order.email}",
-            "",
-            f"Сумма заказа: {order_price}",
-            f"Способ оплаты: {order.payment_type}",
-            "" "Дата доставки:",
-            f"{order.day} в интервале {order.delivery_time}",
-            "",
-            "Адрес доставки:",
-            ", ".join(
-                filter(
-                    lambda x: x,
-                    (
-                        f"{order.address}",
-                        f"подъезд {order.entrance}" if order.entrance else "",
-                        f"квартира {order.apartment}",
-                        f"этаж {order.floor}",
-                        f"домофон {order.intercom}" if order.intercom else "",
-                    ),
-                )
-            ),
-            f"Комментарий: {order.comment}" if order.comment else "",
-            "",
-            "Позиции заказа:",
-            "\n".join(f"{item.product} - {item.amount} шт." for item in cart_items),
-        )
-    )
+    message = form_order_text_repr(order, "Поступил новый заказ")
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     keyboard.add(types.InlineKeyboardButton("Принять заказ", callback_data=f"order::confirm::{order.id}"))
     keyboard.add(types.InlineKeyboardButton("Это спам", callback_data=f"order::spam::{order.id}"))
--- src/services/store.py
+++ src/services/store.py
@@ -20,3 +20,46 @@ def form_payment_url(order: Order) -> str:
     order.payment = payment
     order.save()
     return url
+
+
+def form_order_text_repr(order: Order, report_prefix: str) -> str:
+    """
+    report_prefix is used to handle context:
+    either this report is for customer eyes or a manager
+    """
+    cart_items = order.cartitem_set.prefetch_related("product").all()
+    order_price = calculate_price(cart_items)
+    return "\n".join(
+        (
+            f"{report_prefix}: №{order.id}",
+            "",
+            f"От: {order.name}",
+            "",
+            "Контакты:",
+            f"Телефон: {order.phone}",
+            f"E-mail: {order.email}",
+            "",
+            f"Сумма заказа: {order_price}",
+            f"Способ оплаты: {order.payment_type}",
+            "" "Дата доставки:",
+            f"{order.day} в интервале {order.delivery_time}",
+            "",
+            "Адрес доставки:",
+            ", ".join(
+                filter(
+                    lambda x: x,
+                    (
+                        f"{order.address}",
+                        f"подъезд {order.entrance}" if order.entrance else "",
+                        f"квартира {order.apartment}",
+                        f"этаж {order.floor}",
+                        f"домофон {order.intercom}" if order.intercom else "",
+                    ),
+                )
+            ),
+            f"Комментарий: {order.comment}" if order.comment else "",
+            "",
+            "Позиции заказа:",
+            "\n".join(f"{item.product} - {item.amount} шт." for item in cart_items),
+        )
+    )

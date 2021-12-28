435f8881e66e3018b5db66e1b16155af46e02c70
sealwing <sealinsky@gmail.com>
2021-12-28 16:16:04 +0500

    bot command fix

--- src/messenger_bot/views.py
+++ src/messenger_bot/views.py
@@ -10,5 +10,5 @@ def handle_hook(request: HttpRequest):
     if request.method == "POST" and request.content_type == "application/json":
         data = json.loads(request.body)
         upd = telebot.types.Update.de_json(data)
-        bot.process_command([upd])
+        bot.process_new_updates([upd])
     return JsonResponse({"success": True})
